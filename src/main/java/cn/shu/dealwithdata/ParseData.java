82ba43822c3798c2006cc0f1df6564ee625ba6ad
Shu <1129220400@qq.com>
2017-03-18 22:00:04 +0800

    更新后

--- src/main/java/cn/shu/dealwithdata/ParseData.java
+++ src/main/java/cn/shu/dealwithdata/ParseData.java
@@ -1,12 +1,12 @@
 package cn.shu.dealwithdata;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFTable;
-import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.apache.poi.xwpf.usermodel.*;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.lang.reflect.Field;
+import java.math.BigInteger;
 import java.util.List;
 
 /**
@@ -19,7 +19,7 @@ public class ParseData {
 
     public static <T> void newWordDoc(Class<T> tClass, List<T> objList, String filename) throws Exception {
         //构建文档对象
-        XWPFDocument document = createDoc();
+        XWPFDocument document = createDoc(tClass);
 
         //构建表格
         XWPFTable xTable = createTable(tClass, objList, document);
@@ -30,23 +30,51 @@ public class ParseData {
 
         //写入文件
         String separator = File.separator;
-        FileOutputStream fos = new FileOutputStream(new File("d:" + separator + "java&wordTest", filename + ".doc"));
+        FileOutputStream fos = new FileOutputStream(new File("d:" + separator + "java&wordTest", filename + ".docx"));
         document.write(fos);
         fos.close();
     }
 
 
     //创建文档对象
-    private static <T> XWPFDocument createDoc() {
+    private static <T> XWPFDocument createDoc(Class<T> tClass) {
         XWPFDocument document = new XWPFDocument();
+        XWPFParagraph title = document.createParagraph();
+        //设置标题对齐方式
+        title.setAlignment(ParagraphAlignment.CENTER);
+        title.setVerticalAlignment(TextAlignment.TOP);
+
+        XWPFRun xr = title.createRun();
+        //设置字体是否加粗
+        xr.setBold(true);
+        xr.setFontSize(20);
+        String name = tClass.getName();
+        xr.setText(name.substring(name.lastIndexOf(".") + 1, name.length()) + "表");
         return document;
     }
 
 
-    //构建表格
+    //构建表格结构
     private static <T> XWPFTable createTable(Class<T> tClass, List<T> objList, XWPFDocument xd) {
         tableRowsColumns(tClass, objList); //设置表格的行数和列数
         XWPFTable xTable = xd.createTable(rows + 1, columns); //需要多构造一行，用于表头
+
+        //表格属性
+        CTTblPr tblPr = xTable.getCTTbl().getTblPr();
+        tblPr.getTblW().setType(STTblWidth.DXA);
+        tblPr.getTblW().setW(BigInteger.valueOf(8300));
+
+        // 设置上下左右四个方向的距离，可以将表格撑大
+        xTable.setCellMargins(100, 100, 100, 100);
+
+        // 表格
+        List<XWPFTableCell> tableCells = xTable.getRow(0).getTableCells();
+        XWPFTableCell cell = tableCells.get(0);
+//        相当于合并单元格
+//        XWPFParagraph newPara = new XWPFParagraph(cell.getCTTc().addNewP(), cell);
+//        XWPFRun run = newPara.createRun();
+//        //内容居中显示
+//        newPara.setAlignment(ParagraphAlignment.CENTER);
 
         return xTable;
     }
@@ -82,7 +110,7 @@ public class ParseData {
             T obj = objList.get(rows -1);
             xRow = xTable.getRow(rows); //获取最后一行
             for (int j = 0; j < columns; ++j) {
-                xRow.getCell(j).setText("\r\n" + fields[j].get(obj) + "");
+                xRow.getCell(j).setText(fields[j].get(obj) + "");
             }
         } catch (IllegalAccessException e) {
             e.printStackTrace();
@@ -96,4 +124,7 @@ public class ParseData {
         rows = objList.size();
         columns = fields.length;
     }
+
+    //设置表格风格
+//    private
 }
